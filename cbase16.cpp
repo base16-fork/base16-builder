e9d57eabd2fdc5fb9959dfaefe8844809469bd20
Khue Nguyen <Z5483Y@gmail.com>
2021-05-21 21:22:20 -0400

    feat(base16): show git fetching and checkout progress

--- cbase16.cpp
+++ cbase16.cpp
@@ -5,12 +5,54 @@
 #include <fstream>
 #include <iostream>
 
+int fetch_progress(const git_transfer_progress *stats, void *payload)
+{
+	int fetch_percent = stats->received_objects / stats->total_objects * 100;
+	int index_percent = stats->indexed_objects / stats->total_objects * 100;
+	int kbytes = stats->received_bytes / 1024;
+
+	printf("\rFetch progress: network %d%% (%d kb, %d/%d) / index %d%% (%d/%d)",
+	       fetch_percent, kbytes,
+	       stats->received_objects, stats->total_objects,
+	       index_percent,
+	       stats->indexed_objects, stats->total_objects);
+
+	fflush(stdout);
+
+	if (index_percent == 100) {
+		std::cout << ", done." << std::endl;
+		fflush(stdout);
+	}
+
+	return 0;
+}
+
+void
+checkout_progress(const char *path, std::size_t cur, std::size_t tot, void *payload)
+{
+	int percent = cur / tot * 100;
+	printf("\rCheckout progress: %d%% (%zu/%zu)", percent, cur, tot);
+	fflush(stdout);
+
+	if (percent == 100) {
+		std::cout << ", done." << std::endl;
+		fflush(stdout);
+	}
+}
+
 int
 do_clone(const char *path, const char *url)
 {
 	git_libgit2_init();
+
 	git_repository *repo = NULL;
-	int ret = git_clone(&repo, url, path, NULL);
+	git_clone_options opts = GIT_CLONE_OPTIONS_INIT;
+
+	opts.checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
+	opts.checkout_opts.progress_cb = checkout_progress;
+	opts.fetch_opts.callbacks.transfer_progress = fetch_progress;
+
+	int ret = git_clone(&repo, url, path, &opts);
 	git_repository_free(repo);
 	return ret;
 }
@@ -31,7 +73,9 @@ clone(std::string dir, std::string source)
 		}
 
 		for (int i = 0; i < token_key.size(); ++i) {
+			printf("Cloning %s (%s):\n", token_key[i].c_str(), token_value[i].c_str());
 			do_clone((dir + token_key[i]).c_str(), token_value[i].c_str());
+			std::cout << std::endl;
 		}
 	} else {
 		std::cerr << "error: cannot read " << source << std::endl;
