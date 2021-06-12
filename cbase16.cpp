99a485b48ce38192179bf908d422cc8a73674133
Khue Nguyen <Z5483Y@gmail.com>
2021-06-12 01:54:48 -0400

    revert(base16): remove die function

--- cbase16.cpp
+++ cbase16.cpp
@@ -30,7 +30,6 @@ std::vector<std::string> opt_templates;
 std::filesystem::path opt_output = "output";
 std::filesystem::path opt_cache_dir;
 
-void die(int, std::string);
 void clone(std::string, std::string);
 void update(void);
 std::vector<Template> get_templates(void);
@@ -38,13 +37,6 @@ std::vector<Scheme> get_schemes(void);
 std::vector<int> hex_to_rgb(std::string);
 void replace_all(std::string &, const std::string &, const std::string &);
 void build(void);
-
-void
-die(int e, std::string str)
-{
-	std::cout << str << std::endl;
-	exit(e);
-}
 
 void
 clone(std::string dir, std::string source)
@@ -69,7 +61,8 @@ clone(std::string dir, std::string source)
 		}
 
 	} else {
-		die(1, "error: cannot read " + source);
+		std::cout << "error: cannot read " + source << std::endl;
+		exit(1);
 	}
 }
 
@@ -92,7 +85,8 @@ update(void)
 		file << source.c_str();
 		file.close();
 	} else {
-		die(1, "error: fail to write sources.yaml to " + opt_cache_dir.string());
+		std::cout << "error: fail to write sources.yaml to " << opt_cache_dir << std::endl;
+		exit(1);
 	}
 
 	git_libgit2_init();
@@ -345,29 +339,34 @@ main(int argc, char *argv[])
 		}
 	}
 
-	if (argv[optind] == NULL)
-		die(1, "error: argument is null");
+	if (argv[optind] == NULL) {
+		std::cout << "error: argument is null" << std::endl;
+		return 1;
+	}
 
-	if (std::strcmp(argv[optind], "update") == 0)
+	if (std::strcmp(argv[optind], "update") == 0) {
 		update();
-	else if (std::strcmp(argv[optind], "build") == 0)
+	} else if (std::strcmp(argv[optind], "build") == 0) {
 		build();
-	else if (std::strcmp(argv[optind], "version") == 0)
-		die(0, "cbase16-0.2.0\n");
-	else if (std::strcmp(argv[optind], "help") == 0)
-		die(0, "usage: cbase16 [command] [options]\n"
-		       "command:\n"
-		       "   update  -- fetch all necessary sources for building\n"
-		       "   build   -- generate colorscheme templates\n"
-		       "   version -- display version\n"
-		       "   help    -- display usage message\n"
-		       "options:\n"
-		       "   -c -- specify cache directory\n"
-		       "   -s -- only build specified schemes\n"
-		       "   -t -- only build specified templates\n"
-		       "   -o -- specify output directory");
-	else
-		die(1, "error: invalid command: " + (std::string)argv[optind]);
+	} else if (std::strcmp(argv[optind], "version") == 0) {
+		std::cout << "cbase16-0.2.0" << std::endl;
+	} else if (std::strcmp(argv[optind], "help") == 0) {
+		std::cout << "usage: cbase16 [command] [options]\n"
+			     "command:\n"
+			     "   update  -- fetch all necessary sources for building\n"
+			     "   build   -- generate colorscheme templates\n"
+			     "   version -- display version\n"
+			     "   help    -- display usage message\n"
+			     "options:\n"
+			     "   -c -- specify cache directory\n"
+			     "   -s -- only build specified schemes\n"
+			     "   -t -- only build specified templates\n"
+			     "   -o -- specify output directory"
+			  << std::endl;
+	} else {
+		std::cout << "error: invalid command: " << argv[optind] << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
